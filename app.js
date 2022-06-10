a31b1cf066707aaa3ed6aea95e4aa851a447c650
Kemal K <kemalkaracic@gmail.com>
2022-06-10 11:01:22 +0200

    Changing app.js and home.ejs

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'Pages'));
 
 app.get('/', (req, res) => {
     console.log("Hello from the other side");
-    res.send("home.ejs");
+    res.render('home');
 })
 
 
