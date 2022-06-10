d3e9a929da4df29cfad570083bfbf6f2d7ed4a6a
Kemal K <kemalkaracic@gmail.com>
2022-06-10 10:42:23 +0200

    Adding local host port and installing nodemon

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const express = require("express");
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log("Hello from the other side");
+    res.send("home.ejs");
+})
+
+
+app.listen(3000, () => {
+    console.log("Serving on the port 3000");
+})
