11d30db225c0eb85265a9f9297953f7af93eed56
Kemal K <kemalkaracic@gmail.com>
2022-06-12 13:49:24 +0200

    Adding solar model

--- Project_model/solar_model.js
+++ Project_model/solar_model.js
@@ -1,2 +1,19 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+
+
+
+const ProjectModel = new Schema({
+    Project_name: string,
+    Investor: string,
+    Location: string,
+    Location_coordinates: string,
+    Annual_consumption: Number,
+    Inverter_type: string,
+    Number_of_inverters: Number,
+    Maximum_output_power: Number,
+    Solar_panel_type: string,
+    Number_of_strings: Number,
+
+})
