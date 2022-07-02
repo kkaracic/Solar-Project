31416136e5ce9de7a2a0f0065a4f6a46e82d5638
Kemal K <kemalkaracic@gmail.com>
2022-07-02 12:39:03 +0200

    swaping res and req

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
 
 
-
+mongoose.connect('mongodb://localhost:27017/sollar-pr');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Pages'));
