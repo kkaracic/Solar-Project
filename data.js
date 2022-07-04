c63b4cc0f75974f650dce20a8cb982ac1bff3224
Kemal K <kemalkaracic@gmail.com>
2022-07-04 18:58:00 +0200

    Updating

---
+++ data.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const file = fs.readFileSync("ekvator.txt");
+
+const array = file.toString().split('\n').slice(1);
+
+
+let objects = []
+
+for (let i = 0; i < array.length; i++) {
+    let niz = array[i].split(',');
+    objects.push({
+        Latitude: niz[0],
+        Longitude: niz[1],
+        Irr: niz[2]
+    });
+}
+
+console.log(objects);
